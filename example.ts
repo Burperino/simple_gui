eaa1471fa69f503f76a78fe32fbfd2ff194da263
Burperino <102432103+Burperino@users.noreply.github.com>
2022-07-22 20:27:28 +0200

    Update example.ts

--- example.ts
+++ example.ts
@@ -1,4 +1,4 @@
-import {window} from '';
+import {window} from 'https://raw.githubusercontent.com/Burperino/simple_gui/base/mod.ts';
 
 //   width, height, file path, title
 window(800, 600, "test.html", "Test");
