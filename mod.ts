1bf1a2bb477241a49bbe4f6b79911f53313556c9
Burperino <102432103+Burperino@users.noreply.github.com>
2022-07-22 20:06:30 +0200

    root

---
+++ mod.ts
@@ -0,0 +1,19 @@
+// const fileContent = await Deno.readFile(".\\deps\\window.exe");
+// const str = `
+// const data: Uint8Array = new Uint8Array([${fileContent}]);
+// export {data};
+// `;
+// Deno.writeTextFile(".\\deps\\window.ts", str);
+
+import {data} from "./windowdata.ts";
+
+async function window(width: number, height: number, file: string, title: string) {
+    await Deno.writeFile("temp.exe", data);
+    const proc = Deno.run({
+        cmd: [".\\temp.exe", `${width}`, `${height}`, `${file}`, `${title}`]
+    });
+    await proc.status();
+    await Deno.remove("temp.exe");
+}
+
+window(800, 600, "test.html", "Test");
