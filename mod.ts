56c816a850cd0a2a2f3332f49cdf842cddeb3fd0
Burperino <102432103+Burperino@users.noreply.github.com>
2022-07-22 20:07:24 +0200

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,10 +1,3 @@
-// const fileContent = await Deno.readFile(".\\deps\\window.exe");
-// const str = `
-// const data: Uint8Array = new Uint8Array([${fileContent}]);
-// export {data};
-// `;
-// Deno.writeTextFile(".\\deps\\window.ts", str);
-
 import {data} from "./windowdata.ts";
 
 async function window(width: number, height: number, file: string, title: string) {
